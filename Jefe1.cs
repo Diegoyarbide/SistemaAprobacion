a016f21c3306e4133069b0ce5529b8c8fd47f52c
Diego <doyarbidep325@ulacit.ed.cr>
2020-11-17 22:32:27 -0600

    Commit Staged

---
+++ Jefe1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaAprobacion
+{
+    public partial class Jefe1 : Form
+    {
+        
+        public Jefe1()
+        {
+            InitializeComponent();
+        }
+
+        private void Jefe1_Load(object sender, EventArgs e)
+        {
+            txt2.Text = aprobacion.sendtext;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("El Monto Ha sido aprovado, enviando a siguiente nivel de revision");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            Form Negado = new MsjdenegadoJefe1();
+            Negado.Show();
+            this.Hide();
+        }
+    }
+}
